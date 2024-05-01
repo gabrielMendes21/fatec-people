32e72c1a8c5c9b4187623429aff33bff5cc93998
Gabriel Mendes <gabriel.26056@gmail.com>
2024-05-01 19:40:46 -0300

    update

---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+    localStorage.setItem('user', 'be151fe1-7205-4225-b9f2-91ee84e2dc79')
+})
---
+++ src/views/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FATEC play</title>
+
+    <!-- JS -->
+    <script src="../js/main.js"></script>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../css/main.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <img src="../../assets/logo.png" alt="logo">
+        </header>
+        <div class="vertical-line"></div>
+        <aside class="playlists">
+            <div class="add">
+                <img src="../../assets/add-circle.svg" alt="Sinal de mais">
+                <span>Adicionar playlist</span>
+            </div>
+        </aside>
+    </div>
+</body>
+</html>

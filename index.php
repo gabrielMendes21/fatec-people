cfb5780033369feb5018226953bc510db42f1947
JoaoVitordSRodrigues <joaovitor10sr@gmail.com>
2024-05-02 01:12:50 -0300

    inicio de layout

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FATEC play</title>
+
+    <!-- JS -->
+    <script src="../js/main.js"></script>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <img src="./assets/logo.png" alt="logo">
+        </header>
+        <div class="vertical-line"></div>
+        <div class="content">
+            <aside>
+                <div class="add">                    
+                    <img src="./assets/add-circle.svg" alt="Sinal de mais">
+                    <span>Adicionar música</span>
+                </div>
+            </aside>
+
+            <main>         
+                <div class="card-music">
+                    <div class="operations">
+                        <button>EDITAR</button>
+                        <button>EXCLUIR</button>
+                    </div>
+                    <iframe width="330" height="200" src="https://www.youtube.com/embed/uDsljnSyk3g" frameborder="0" allowfullscreen></iframe>
+                    <p>NOME DA MUSICA</p>
+                    <p>MAIS INFO</p>
+                </div>
+
+                <div class="card-music">
+                    <div class="operations">
+                        <button>EDITAR</button>
+                        <button>EXCLUIR</button>
+                    </div>
+                    <iframe width="330" height="200" src="https://www.youtube.com/embed/uDsljnSyk3g" frameborder="0" allowfullscreen></iframe>
+                    <p>NOME DA MUSICA</p>
+                    <p>MAIS INFO</p>
+                </div>
+
+                <div class="card-music">
+                    <div class="operations">
+                        <button>EDITAR</button>
+                        <button>EXCLUIR</button>
+                    </div>
+                    <iframe width="330" height="200" src="https://www.youtube.com/embed/uDsljnSyk3g" frameborder="0" allowfullscreen></iframe>
+                    <p>NOME DA MUSICA</p>
+                    <p>MAIS INFO</p>
+                </div>
+
+                
+                        
+            </main>
+        </div>
+    </div>
+</body>
+</html>

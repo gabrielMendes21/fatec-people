3693b93ae7ac152968c4a4e10d6872eed9fdbf94
Gabriel Mendes <gabriel.26056@gmail.com>
2024-05-02 21:21:51 -0300

    update

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const addUserButton = document.querySelector('.add');
     const editUserButton = document.querySelector('.edit');
     const closeAddModal = document.querySelector('.close-add');
+    const closeEditModal = document.querySelector('.close-edit');
     const addUserModal = document.querySelector('.modal-cadastro');
     const editUserModal = document.querySelector('.modal-edicao');
     addUserModal.classList.add("invisible");
+    editUserModal.classList.add("invisible");
 
     addUserButton.addEventListener("click", (event) => {
         addUserModal.classList.remove("invisible");
@@ -15,10 +17,10 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 
     editUserButton.addEventListener("click", (event) => {
-        addUserModal.classList.remove("invisible");
+        editUserModal.classList.remove("invisible");
     })
 
-    closeModalButton.addEventListener("click", () => {
+    closeEditModal.addEventListener("click", () => {
         editUserModal.classList.add("invisible");
     })
 })
